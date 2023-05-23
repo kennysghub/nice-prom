f3a9278006b8e3af8f7bf954357cda36a37c45ec
Kenny <kennykn@outlook.com>
2023-05-23 16:01:33 -0400

    working on histogram

--- registry.ts
+++ registry.ts
@@ -1,32 +1,5 @@
-//require prom-client: prometheus for node.js that supports historgram, summaries, gauges and counters
-//works with nice-grpc-prometheus
-// import promClient from 'prom-client';
-//globalRegistry is assinged to an instance of Registery
-// const globalRegistry= promClient.register;
-//Registry is the class in prom-client (lambda Registry)
-const {Registry} = promClient;
-// export default globalRegistry;
+import {register as globalRegistry, Registry} from 'prom-client';
+import {registry as niceGrpcRegistry} from 'nice-grpc-prometheus';
 
-
-
-
-//console.log(`globalRegistry: ${globalRegistry}, Registry: ${Registry}`)
-
-//require nice-grpc-prometheus
-// import niceGrpcClient from 'nice-grpc-prometheus';
-// const niceGrpcRegistry = niceGrpcClient.registry;
-
-//merge niceGrpcRegistry with global (use await to export all metrics)
-// export const mergedRegistry = Registry.merge([globalRegistry, niceGrpcRegistry]);
-
-//export
-
-
-
-import * as promClient from 'prom-client';
-import { registry as niceGrpcRegistry } from 'nice-grpc-prometheus';
-
-const globalRegistry = promClient.register;
-const mergedRegistry = promClient.Registry.merge([globalRegistry, niceGrpcRegistry]);
-
-export default mergedRegistry;
+export const mergedRegistry = Registry.merge([globalRegistry, niceGrpcRegistry]);
+export default niceGrpcRegistry;
--- server.ts
+++ server.ts
@@ -3,16 +3,50 @@ import {prometheusServerMiddleware} from 'nice-grpc-prometheus';
 //import * as serverRegistry from './registry';
 import { DeepPartial, GreetRequest, GreetResponse, GreetServiceDefinition, GreetServiceImplementation } from './compiled_proto/test'
 import { ServerCredentials } from '@grpc/grpc-js';
+import mergedRegistry from './registry';
+import { Counter, Histogram } from 'prom-client';
 
+// Define the metrics
+const counterStartedTotal = new Counter({
+  name: 'grpc_server_started_total',
+  help: 'Total number of RPCs started on the server',
+});
+
+const counterHandledTotal = new  Counter({
+  name: 'grpc_server_handled_total',
+  help: 'Total number of RPCs completed on the server, regardless of success or failure',
+});
 // GreetService Impl object is defined with an `async` keyword, indiciating that it returns a -> PROMISE. 
+const histogramHandlingSeconds = new Histogram({
+    name: 'grpc_server_handling_seconds',
+    help: 'Histogram of response latency (seconds) of gRPC that had been application-level handled by the server',
+    buckets: [0.1, 0.5, 1, 2, 5],
+  });
 // Make sure the `greetings` method is using the -> promise-based approach instead of the callback approach. 
 
 const GreetServiceImpl: GreetServiceImplementation = {
-    async greetings(request: GreetRequest): Promise<DeepPartial<GreetResponse>> {
+    async greetings(request: GreetRequest,context:CallContext): Promise<DeepPartial<GreetResponse>> {
       try {
         const response: GreetResponse = {
           Goodbye: 'bye!',
         };
+        counterStartedTotal.inc();
+        counterHandledTotal.inc();
+        console.log('histogram---', histogramHandlingSeconds)
+        console.log(Histogram)
+        console.log(counterStartedTotal)
+        console.log(counterHandledTotal)
+        counterStartedTotal.inc();
+    // Your gRPC server logic here
+    counterHandledTotal.inc();
+    counterHandledTotal.inc();
+    counterHandledTotal.inc();
+    counterHandledTotal.inc();
+    counterHandledTotal.inc();
+    console.log('look---->', counterHandledTotal)
+
+    // Start measuring the response handling time
+    counterHandledTotal.inc();
         return response;
       } catch (err) {
         console.error(err);
@@ -21,10 +55,27 @@ const GreetServiceImpl: GreetServiceImplementation = {
     },
   };
   
+  const allMetrics = async () => {
+    try {
+      const getMetrics = mergedRegistry.metrics();
+      const metrics = await getMetrics;
+      console.log(metrics);
+    
+      return metrics;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  allMetrics();
 
+const metrics =  mergedRegistry.metrics();
+metrics.then(res => console.log(res))
 
+  
 const server = createServer()
 server.use(prometheusServerMiddleware())
     server.add(GreetServiceDefinition, GreetServiceImpl)
+
 
 server.listen('127.0.0.1:3500', ServerCredentials.createInsecure())
